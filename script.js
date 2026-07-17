b91ffe4d540a80e8cdc6867105bfb342c298d473
mehaksheikhh786-spec <mehaksheikhh786@gmail.com>
2026-07-17 05:43:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-function startSurprise(){
+function checkPassword(){
 
-    alert(
-        "YAY JIAA!! 💗 Your magical birthday surprise is starting! 🎀"
-    );
+let password = document.getElementById("password").value;
+
+if(password === "2009"){
+
+document.getElementById("passwordBox").style.display="none";
+
+document.getElementById("welcomeBox").classList.remove("hidden");
+
+}
+
+else{
+
+document.getElementById("message").innerHTML =
+"🥺 Wrong password, try again 💗";
+
+}
+
+}
+
+
+function nextPage(){
+
+alert("Next surprise is coming 💌");
 
 }
